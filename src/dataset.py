80b6a5e24230089333c88e828976170fe0c4ab2a
Shaoxiang Tan <tsx19921030@gmail.com>
2024-10-05 23:14:13 +0200

    update files

--- src/dataset.py
+++ src/dataset.py
@@ -1,6 +1,8 @@
 import torch
 from torch.utils.data import Dataset
 from PIL import Image
+import os
+from path import ROOT_DIR
 
 class MyData(Dataset):
 
@@ -16,11 +18,11 @@ class MyData(Dataset):
 
     # load data via idx and image preprocessing
     def __getitem__(self, idx):
-        image_path = self.image_paths[idx]
+        image_path = os.path.join(ROOT_DIR, "data", "imgs", self.image_paths[idx])
         label = self.labels[idx]
         image = Image.open(image_path).convert("RGB")
         
         if self.transform:
             image = self.transform(image)
         
-        return image, torch.tensor(label, dtype=torch.float32)
+        return image.type(torch.float32), torch.tensor(label, dtype=torch.float32)
